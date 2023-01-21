0549dd4370a85994fb29c76d983a3a86e39c62dd
John Frick <johnfrickr@gmail.com>
2023-01-21 15:49:13 -0600

    Added Pseudocode and background img

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("hello")
+//INITIAL PSEUDOCODE 
+
+// 1. Format the HTML to have a main background that is static
+// 2. make a footer that will contain the buttons the user will use to "interact"
+// 3. find an image for the tomagatchi and center it on the background
+// 4. give the appropriate class names to the HTML to be able to add JS methods/functions to manipulate them
+// 5. Manipulate the CSS to get the pet to do something on the page 
+// 6. Style the page using CSS and make it look formatted well 
+// 7. add the visual value trackers to the page as well, (hunger, boredmom, tiredness). 
+
+// FUNCTIONS PSEUDOCODE 
+// 1. I need there to be a set timer running in the background
+// 2. I will need to make the age of the pet equal to the timer 
+// 3. I will need to create three visual values 
+
